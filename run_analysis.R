464eafe50a07ae852cab4365e2aa97533b51bf99
Alex Solomin <solomin@gmail.com>
2014-06-14 19:16:05 +0400

    refactoring, script is completed

--- run_analysis.R
+++ run_analysis.R
@@ -1,63 +1,37 @@
-GetRequiredReatures <- function(root.dir) {
-	file.name <- paste(root.dir, "/features.txt", sep = "")
-	features <- read.table(file.name, col.names = c("Number", "Description"), stringsAsFactors = FALSE)
-	features <- subset(features, grepl("mean\\(\\)|std\\(\\)", Description))
-	features
-}
 
-ReadActivitiesLabels <- function(root.dir) {
-	file.name <- paste(root.dir, "/activity_labels.txt", sep = "")
-	activities <- read.table(file.name, col.names = c("Activity", "Label"), stringsAsFactors = FALSE)
-	activities
-}
-
-ReadXdata <- function(root.dir, type, cols) {
-	file.name <- paste(root.dir, "/", type, "/X_", type, ".txt",sep = "")
-	data <- read.table(file.name, header = FALSE)
-	data[, cols]
-}
-
-ReadSubjects <- function(root.dir, type) {
-	file.name <- paste(root.dir, "/", type, "/subject_", type, ".txt",sep = "")
-	read.table(file.name, header = FALSE, col.names = c("Subject"))
-}
-
-ReadActivities <- function(root.dir, type) {
-	file.name <- paste(root.dir, "/", type, "/y_", type, ".txt",sep = "")
-	activities <- read.table(file.name, col.names = c("Activity"))
-	activities
-}
+library('plyr')
 
 ReadDataByType <- function(root.dir, type, features, activities.labels) {
-	subjects <- ReadSubjects(root.dir, type)	
-	activities <- sapply(ReadActivities(root.dir, type), function(a) activities.labels$Label[a])
+  file.name <- paste(root.dir, "/", type, "/subject_", type, ".txt",sep = "")
+  subjects <- read.table(file.name, header = FALSE, col.names = c("Subject"))
+  
+  file.name <- paste(root.dir, "/", type, "/y_", type, ".txt",sep = "")
+  activities <- read.table(file.name, col.names = c("Activity"))
+	activities <- sapply(activities, function(a) activities.labels$Label[a])
 
-	data <- ReadXdata(root.dir, type, features$Number)
+  file.name <- paste(root.dir, "/", type, "/X_", type, ".txt",sep = "")
+  data <- read.table(file.name, header = FALSE)
+  data <- data[, features$Number]  
 	colnames(data) <- features$Description
-	data <- cbind(subjects, activities, data)
-	data
+
+	cbind(subjects, activities, data)
 }
 
-ReadData <- function(root.dir) {
-	message("reading a features list...")
-	features <- GetRequiredReatures(root.dir)
-	
-	message("reading an activities list...")
-	activities.labels <- ReadActivitiesLabels(root.dir)
-
-	message("reading tests data...")
-	test <- ReadDataByType(root.dir, "test", features, activities.labels)
-	
-	message("reading train data...")
-	train <- ReadDataByType(root.dir, "train", features, activities.labels)
-	
-	data <- rbind(test, train)
-	data
-}
-
-RunAnalysis <- function(root.dir) {
-	data <- ReadData(root.dir)
-	
-	message("processing data...")
-	#sapply(split(data, list(data$Subject, data$Activity)), function(a) sapply(a[c(-1, -2)], mean))
+RunAnalysis <- function(root.dir = "UCI HAR Dataset") {  
+  file.name <- paste(root.dir, "/features.txt", sep = "")
+  features <- read.table(file.name, col.names = c("Number", "Description"), 
+                         stringsAsFactors = FALSE)
+  features <- subset(features, grepl("mean\\(\\)|std\\(\\)", Description))
+  
+  
+  file.name <- paste(root.dir, "/activity_labels.txt", sep = "")
+  activities.labels <- read.table(file.name, col.names = c("Activity", "Label"), 
+                                  stringsAsFactors = FALSE)
+    
+  
+  data <-
+    rbind(ReadDataByType(root.dir, "test", features, activities.labels), 
+          ReadDataByType(root.dir, "train", features, activities.labels))
+  
+	ddply(data, .(Subject, Activity), numcolwise(mean))
 }
